0f6d7d7a6fd27481387d86af7f6ad9a00ead9cc5
nilimakafe2018 <nilimakafle2018@gmail.com>
2026-02-27 12:09:54 -0600

    Completed adding Controller for User with CRUD endpoints

---
+++ backend-project/src/main/java/com/example/backend/project/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.example.backend.project.controller;
+import com.example.backend.project.model.User;
+import com.example.backend.project.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping ("/api/users")
+@CrossOrigin
+
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //create a new user
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    //read all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    //read one user by id
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+
+    //update user
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        User user = userRepository.findById(id).orElseThrow();
+        user.SetName(updatedUser.getName());
+        user.SetEmail(updatedUser.getEmail());
+        user.SetPassword(updatedUser.getPassword());
+        return userRepository.save(user);
+    }
+
+    //delete user
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
