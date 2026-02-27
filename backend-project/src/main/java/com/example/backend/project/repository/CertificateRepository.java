a3ea943f8a9a7c08978fe600f83951c05093ba6d
nilimakafe2018 <nilimakafle2018@gmail.com>
2026-02-27 14:03:47 -0600

    added repository for certificate

--- backend-project/src/main/java/com/example/backend/project/repository/CertificateRepository.java
+++ backend-project/src/main/java/com/example/backend/project/repository/CertificateRepository.java
@@ -1,7 +1,3 @@
 package com.example.backend.project.repository;
 import com.example.backend.project.model.Certificate;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CertificateRepository extends JpaRepository<Certificate,Long>{
-
-}
