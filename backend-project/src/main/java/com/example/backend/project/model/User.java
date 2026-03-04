2b8ce5f6f3cdfb32bc4b3387f82c46eac8cd7bd0
nilimakafe2018 <nilimakafle2018@gmail.com>
2026-03-03 19:14:05 -0600

    Completed Milestone 2B PERSISTENT API

--- backend-project/src/main/java/com/example/backend/project/model/User.java
+++ backend-project/src/main/java/com/example/backend/project/model/User.java
@@ -1,7 +1,7 @@
 package com.example.backend.project.model;
 import jakarta.persistence.*;
-//import jakarta.persistence.OneToOne;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+//import jakarta.persistence.OneToOne;
 
 @Entity
 @Table(name = "users")
